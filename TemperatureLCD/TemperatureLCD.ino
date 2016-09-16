40d4b71a9f10e8919a79d4668f0f1c3f3e81ad76
Chris Nethercott <c.nethercott@icloud.com>
2016-09-16 10:32:01 +0100

    Added a load of stuff

--- TemperatureLCD/TemperatureLCD.ino
+++ TemperatureLCD/TemperatureLCD.ino
@@ -10,60 +10,55 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 OneWire oneWire(6);
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensors(&oneWire);  
+DeviceAddress black = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 }; //Black Address
+DeviceAddress red   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; //Red Address
+DeviceAddress green = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E }; //Green Address
+String FirstLineofLCD = "DGS F24";
 
-//Sets the addresses for each probe on the one wire bus
-DeviceAddress black_probe = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 }; //Black Address
-DeviceAddress red_probe   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; //Red Address
-DeviceAddress green_probe = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E }; //Green Address
-
-void setup()
-{
-  sensors.begin();
-  sensors.setResolution(black_probe, 10);
-  sensors.setResolution(red_probe, 10);
-  sensors.setResolution(green_probe, 10);
-  pinMode(switchPin, INPUT);
+void setup() {
+  setupSensors();
+  lcd.begin();
+  lcd.backlight();
 }
 
-void loop()
-{
-  //delay(delay_time);
+void setupSensors() {
+  sensors.begin();
+  sensors.setResolution(black, 10);
+  sensors.setResolution(red, 10);
+  sensors.setResolution(green, 10);
+}
+
+void loop() {
   sensors.requestTemperatures();
 
   delay(2000);
   lcd.begin(16, 2);
-  lcd.print("DGS Racing");
+  lcd.print(FirstLineofLCD);
   lcd.setCursor(0, 1);
-  lcd.print("Black = ");
-  printTemperature(black_probe);
+  lcd.print("Black = " + printTemperature(black));
 
   delay(2000);
   lcd.clear();
-  lcd.print("DGS Racing");
+  lcd.print(FirstLineofLCD);
   lcd.setCursor(0, 1);
-  lcd.print("Red   = ");
-  printTemperature(red_probe);
-
+  lcd.print("Red   = " + printTemperature(red));
+  
   delay(2000);
   lcd.clear();
-  lcd.print("DGS Racing");
+  lcd.print(FirstLineofLCD);
   lcd.setCursor(0, 1);
-  lcd.print("Green = ");
-  printTemperature(green_probe);
+  lcd.print("Green = " + printTemperature(green));
 }
 
-void printTemperature(DeviceAddress deviceAddress)
-{
+void printTemperature(DeviceAddress deviceAddress) {
 
   float tempC = sensors.getTempC(deviceAddress);
 
-  if (tempC == -127.00)
-  {
-    lcd.print("Failed");
-  }
-  else
-  {
+  if (tempC != -127.00) {
     lcd.print(tempC);
+  }
+  else {
+    lcd.print("Error");
   }
 }
